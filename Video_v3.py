c80bf16fe507decf2a5b98401236fc85290222b0
Григорий Петухов <gapetukhov@miem.hse.ru>
2022-04-27 23:09:31 +0000

    Update Video_v3.py

--- Video_v3.py
+++ Video_v3.py
@@ -4,13 +4,32 @@ import cv2
 import time
 
 def Video():
-    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture(0)
+
+    HIGH_VALUE = 10000
+    WIDTH = HIGH_VALUE
+    HEIGHT = HIGH_VALUE
+
+    capture = cv2.VideoCapture(0)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #focus = int(capture.get(cv2.CAP_PROP_AUTOFOCUS))
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+    #capture.set(cv2.CAP_PROP_FOCUS, 200)
+    capture.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+    width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 
     # Get the Default resolutions
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
+    #frame_width = int(cap.get(3))
+    #frame_height = int(cap.get(4))
     cur_dir = os.getcwd()
+    #os.chdir(cur_dir)
     print(cur_dir)
+    print(width)
+    print(height)
 
     files = "video_timing.txt"
     timing = open(files)
@@ -22,14 +41,14 @@ def Video():
     print(tik)
 
     # Define the codec and filename.
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    #fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     #out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    out = cv2.VideoWriter('video/output.mp4', fourcc, 30.0, (width, height))
 
 
 
-    while(cap.isOpened() ):
-        ret, frame = cap.read()
+    while(capture.isOpened() ):
+        ret, frame = capture.read()
         tok = time.time()
         if ret==True :
             # write the  frame
@@ -43,7 +62,7 @@ def Video():
             break
 
     # Release everything if job is finished
-    cap.release()
+    capture.release()
     out.release()
     cv2.destroyAllWindows()
     time.sleep(4)
