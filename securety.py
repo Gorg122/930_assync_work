9db3aa1cf54d4d7b78d71aa69993623fc227f953
Лоренс Александр Сергеевич <aslorens@edu.hse.ru>
2022-05-17 08:22:05 +0000

    Update securety.py

--- securety.py
+++ securety.py
@@ -1,1 +1,34 @@
+import zipfile
+import resource
+import contextlib
 
+def file_zip_correct():
+    try:
+        archive = zipfile.ZipFile('file.zip', 'r')
+        archive.testzip()
+        archive_list = archive.namelist()
+        for itm in archive_list:
+            if (itm.split('.')[1] == 'bat') or (itm.split('.')[1] == 'inf') or (itm.split('.')[1] == 'mp4'):
+                raise Exception()
+        print('Все файлы соответсвуют безопастности')
+    except zipfile.BadZipFile:
+        print('Неправильный зип файл')
+    except NameError:
+        print('Нет такого файла')
+    except Exception:
+        print('Опасный файл')
+
+
+@contextlib.contextmanager
+def limit(limit, type=resource.RLIMIT_AS):
+    soft_limit, hard_limit = resource.getrlimit(type)
+    resource.setrlimit(type, (limit, hard_limit)) # set soft limit
+    try:
+        yield
+    finally:
+        resource.setrlimit(type, (soft_limit, hard_limit)) # restore
+
+
+with limit(1 << 30): # 1GB 
+    # 1GB  Здесь должна быть по идее разархивация файла
+    print('okey')
